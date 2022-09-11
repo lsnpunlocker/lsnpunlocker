0968a6fd29b203d2f8813782f1174771893978a3
lsnpunlocker <108741910+lsnpunlocker@users.noreply.github.com>
2022-09-11 23:02:40 +0630

    Update imageslider.js

--- js/imageslider.js
+++ js/imageslider.js
@@ -1,19 +1,21 @@
 var i=0;
 var image = [];
-var time = 2500;
+var time = 1500;
 
 
-
-image[1] = "icon/photo/2.png";
-image[2] = "icon/photo/3.png";
-image[3] = "icon/photo/4.png";
-image[4] = "icon/photo/5.png";
-image[5] = "icon/photo/6.png";
-image[6] = "icon/photo/7.png";
-image[7] = "icon/photo/8.png";
-image[8] = "icon/photo/9.png";
-image[9] = "icon/photo/10.png";
-image[10] = "icon/photo/11.png";
+image[0] = "icon/photo/0.png";
+image[1] = "icon/photo/1(2).png";
+image[2] = "icon/photo/1.png";
+image[3] = "icon/photo/2.png";
+image[4] = "icon/photo/3.png";
+image[5] = "icon/photo/4.png";
+image[6] = "icon/photo/5.png";
+image[7] = "icon/photo/6.png";
+image[8] = "icon/photo/7.png";
+image[9] = "icon/photo/8.png";
+image[10] = "icon/photo/9.png";
+image[11] = "icon/photo/10.png";
+image[12] = "icon/photo/11.png";
 
 
 function changeImg() {
